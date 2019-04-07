8b5cc2222bd957d7a693ba81e8e826809b5168f8
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-04-08 03:10:28 +0530

    added redux and redux scaffolding code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux;";
+import reducers from "./reducers";
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View>Hello</View>
+    </Provider>
+  );
+};
+export default App;
